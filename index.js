f8aa2231b926ab9c0775ac8272267ea7fe1b6a7e
Austin Zumbro <azumbro@gmail.com>
2023-04-23 14:50:46 -0700

    shape renders but incorrectly

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const fs = require("fs");
-const { Shape, Triangle, Circle, Square } = require("./lib/shapes");
-const inputHandler = require("./lib/handleInput");
+const CLI = require("./lib/cli");
+const FileWriter = require("./lib/writeToFile");
+
+const cli = new CLI;
+const filewriter = new FileWriter;
 
 function init() {
-    inputHandler();
+    cli.getInput()
+        .then((response) => {
+            let svg = filewriter.parseResponse(response);
+            console.log(svg);
+            return svg;
+        })
+        .then((svg) => {
+            fs.writeFile("./dist/logo.svg", svg, (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+            console.log("Generating SVG...");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 init();
